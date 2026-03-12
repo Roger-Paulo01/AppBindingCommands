97a073fe13bc2ed22a66cdc78743b12a29f02e67
Roger-Paulo01 <rel.cliente.r@gmail.com>
2026-03-11 21:47:35 -0300

    Aula 04

---
+++ AppBindingCommands/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AppBindingCommands.ViewModels
+{
+    public class UsuarioViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke
+                (this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string name = string.Empty;
+
+        public string Name 
+        { 
+            get => name;
+            set
+            {
+                if (name == null)
+                {
+                    return;                    
+                }
+
+                name = value;
+                OnPropertyChanged(nameof(Name));
+                OnPropertyChanged(nameof(DisplayName));
+            }
+        }
+
+        public string DisplayName => $"Nome digitado:{Name}";
+
+
+
+
+
+
+
+
+    }
+}
